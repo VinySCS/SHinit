e4efe171ebac16ba475ddddff1d92efe98259859
VinuxSCS <vinyscsti@gmail.com>
2022-08-16 16:42:00 -0300

    file name changed according the content

--- var.sh
+++ var.sh
@@ -26,3 +26,24 @@ outCat=$(cat /etc/passwd | grep Vinux)
 echo "$outCat"
 #For this case you need to use "" because you are not using to to calc something
 
+echo "\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/"
+
+echo "Par1: $1"
+echo "Par2: $2"
+#parameter 1 and 2 who catch the first and second parameter "./script -par1 -par2"
+
+echo "All Parameters: $*"
+#print all parameters who we typed after ./script
+
+echo "How many parameters? $#"
+#print how many parameters we typed after ./script
+
+echo "Output last command: $?"
+#print what's the status of the last command like: 0(ok) 100(error)
+
+echo "PID: $$"
+#print the process id of the script
+
+echo $0
+#print the name of the script
+
